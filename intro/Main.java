f3487d319b0f0877c25ac0317c110d9d14a2c3ce
Jadon <47489511+gonhunterx@users.noreply.github.com>
2024-01-04 15:56:51 -0500

    Update Main.java

--- intro/Main.java
+++ intro/Main.java
@@ -1,5 +1,9 @@
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] arguments) {
+        int age = 24;
+        
+        System.out.println("I am " + age + " years old");
+        
         System.out.println("Hello World");
     }
 }
